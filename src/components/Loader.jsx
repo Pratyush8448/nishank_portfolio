9c2bbf95f841efdbe4ad211745af98f7a238f386
Pratyush Pulak Nishank <23f2002286@ds.study.iitm.ac.in>
2026-04-05 22:37:51 +0530

    Adjust animation delay in Loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -33,7 +33,7 @@ export default function Loader({ onFinish }) {
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{
-              delay: index * 0.21, 
+              delay: index * 0.3, 
               duration: 0.3,
               ease: "easeOut"
             }}
